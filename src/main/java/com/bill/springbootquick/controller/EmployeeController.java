5ef6401c1ad4e59e89b4331c08691758a8e89ed7
BillLin1209 <zzx123bill@gmail.com>
2020-02-20 23:48:53 +0800

    select all employee -> complete

--- src/main/java/com/bill/springbootquick/controller/EmployeeController.java
+++ src/main/java/com/bill/springbootquick/controller/EmployeeController.java
@@ -1,12 +1,15 @@
 package com.bill.springbootquick.controller;
 
 
+import com.bill.springbootquick.dao.DepartmentDao;
 import com.bill.springbootquick.dao.EmployeeDao;
+import com.bill.springbootquick.entities.Department;
 import com.bill.springbootquick.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 import java.util.List;
@@ -17,6 +20,9 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
+    @Autowired
+    DepartmentDao departmentDao;
+
     @GetMapping("/emps")
     public String lists(Model model){
         Collection<Employee> employees=employeeDao.getAll();
@@ -24,6 +30,15 @@ public class EmployeeController {
 
 
         return "emp/list";
+    }
+
+
+    @GetMapping("/toAddEmpPage")
+    public String toAddEmpPage(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+        return "emp/addEmpPage";
+
     }
 
 }
