2f5d99baf1553a3490e7d39b7bd60d6da2dc084c
BillLin1209 <zzx123bill@gmail.com>
2020-02-22 22:07:58 +0800

    complete "CRU" excluding D，but implement it without RestfulAPI

--- src/main/java/com/bill/springbootquick/controller/EmployeeController.java
+++ src/main/java/com/bill/springbootquick/controller/EmployeeController.java
@@ -8,8 +8,7 @@ import com.bill.springbootquick.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.List;
@@ -24,9 +23,9 @@ public class EmployeeController {
     DepartmentDao departmentDao;
 
     @GetMapping("/emps")
-    public String lists(Model model){
-        Collection<Employee> employees=employeeDao.getAll();
-        model.addAttribute("emps",employees);
+    public String lists(Model model) {
+        Collection<Employee> employees = employeeDao.getAll();
+        model.addAttribute("emps", employees);
 
 
         return "emp/list";
@@ -34,11 +33,50 @@ public class EmployeeController {
 
 
     @GetMapping("/toAddEmpPage")
-    public String toAddEmpPage(Model model){
+    public String toAddEmpPage(Model model) {
         Collection<Department> departments = departmentDao.getDepartments();
-        model.addAttribute("departments",departments);
+        model.addAttribute("departments", departments);
         return "emp/addEmpPage";
 
     }
 
+
+    @GetMapping("/toUpdatePage{id}")
+    public String toUpdatePage(@PathVariable("id") Integer id, Model model) {
+        Employee employee = employeeDao.get(id);
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments", departments);
+        System.out.println("要進行修改的Employee:" + employee);
+        model.addAttribute("emp", employee);
+        return "emp/updatePage";
+    }
+
+    @PostMapping("/addEmp")
+    public String addEmp(Employee employee) {
+        System.out.println("Before" + employee.toString());
+        employeeDao.save(employee);
+        System.out.println("After" + employee.toString());
+        return "redirect:/emps";
+    }
+
+//    @RequestMapping(value = "/updateEmp" ,method = RequestMethod.PUT)
+    @PostMapping("/updateEmp")
+    public String updateEmp(Employee employee){
+//        employee.setId(id);
+        employeeDao.save(employee);
+        return "redirect:/emps";
+//        return "forward:emp/list";
+    }
+
+
+    @GetMapping("/toDeletePage")
+    public String toDeletePage() {
+        return "";
+    }
+
+
+    @GetMapping("/error")
+    public String toErrorPage(){
+        return "404";
+    }
 }
