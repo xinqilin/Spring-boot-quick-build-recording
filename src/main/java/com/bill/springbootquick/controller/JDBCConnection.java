715ca805cc640b09e0f9f48c9dd11baff2ed5796
BillLin1209 <zzx123bill@gmail.com>
2020-02-29 17:14:56 +0800

    throws Exception in jdbcTemplates

--- src/main/java/com/bill/springbootquick/controller/JDBCConnection.java
+++ src/main/java/com/bill/springbootquick/controller/JDBCConnection.java
@@ -7,7 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -21,18 +26,22 @@ public class JDBCConnection {
 
     @ResponseBody
     @GetMapping("/jdbc/id={depid}")
-    public Map<String,Object> jdbc(@PathVariable("depid") Integer depid, Map<String,Object> map){
+    public Map<String, Object> jdbc(@PathVariable("depid") Integer depid, Map<String, Object> map, HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+
+        List<Map<String, Object>> list = new LinkedList<>();
         try {
-            List<Map<String,Object>> list=jdbcTemplate.queryForList("select * from department where id="+depid);
-            return list.get(0);
-        }catch (Exception e){
-            System.err.println("抓到的List是空的and  "+e.getMessage());
-            map.put("db","null");
-            return  map;
+            list = jdbcTemplate.queryForList("select * from department where id=" + depid);
+            if (list.size() != 0) {
+                return list.get(0);
+            }
+
+        } catch (Exception e) {
+            System.err.println("Exception:  " + e.getMessage());
         }
-
+        map.put("error", "沒有符合資料");
+        req.getRequestDispatcher("/src/main/resources/templates/error/404.html").forward(req, res);
+        return null;
     }
-
 
 
 }
