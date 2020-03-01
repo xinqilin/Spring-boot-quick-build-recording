23948768a42eaf48182d6e370c34430da9fe9070
BillLin1209 <zzx123bill@gmail.com>
2020-03-01 19:27:41 +0800

    JPA  --->  start

--- src/main/java/com/bill/springbootquick/mapper/EmployeeMapper.java
+++ src/main/java/com/bill/springbootquick/mapper/EmployeeMapper.java
@@ -12,10 +12,10 @@ public interface EmployeeMapper {
     public Employee getOne(Integer id);
 
     @Options(useGeneratedKeys = true,keyProperty = "id")
-    @Insert("insert into employee(lastname,email,gender,d_id) values (#{lastName},#{email},#{gender},#{d_id})")
+    @Insert("insert into employee(lastname,email,gender,d_id) values (#{lastName},#{email},#{gender},#{departmentId})")
     public void addEmp(Employee employee);
 
-    @Update("update employee set lastname=#{lastName},email=#{email},gender=#{gender},d_id=#{d_id} where id=#{id}")
+    @Update("update employee set lastname=#{lastName},email=#{email},gender=#{gender},d_id=#{departmentId} where id=#{id}")
     public void updateEmp(Employee employee);
 
     @Delete("Delete from employee where id=#{id}")
